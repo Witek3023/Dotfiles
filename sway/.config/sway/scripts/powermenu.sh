263ac6556da9edbc4adb327bc8ad5b3d96b527ad
Witek3023 <witek3023@gmail.com>
2026-05-13 22:17:49 +0200

    fuzzel && powermenu

---
+++ sway/.config/sway/scripts/powermenu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+options="⏻ Power Off\n Reboot\n⏾ Suspend\n󰗽 Logout"
+
+choice=$(echo -e "$options" | fuzzel --dmenu --prompt="Power  " -l 4 -w 25)
+
+case "$choice" in
+    "⏻ Power Off")
+        systemctl poweroff
+        ;;
+    " Reboot")
+        systemctl reboot
+        ;;
+    "⏾ Suspend")
+        systemctl suspend
+        ;;
+    "󰗽 Logout")
+        labwc -E
+        ;;
+    *)
+        exit 0
+        ;;
+esac
